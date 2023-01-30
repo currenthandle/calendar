316f36609e853459555f5a8f0b7504ab14ebb864
Casey <leptone55@gmail.com>
2023-01-29 22:27:05 -0800

    cleaning

--- src/components/WeekCalendar/Events/index.tsx
+++ src/components/WeekCalendar/Events/index.tsx
@@ -33,20 +33,10 @@ const Events = () => {
   // then use the location to calculate the time of the click (day, hour, and minute)
   const handleClick = (e: MouseEvent) => {
     const target = e.target as HTMLElement;
-
-    let firstRowHeight: string | number | undefined =
-      target.style.gridTemplateRows.split(" ")[0];
-    // caclute firstRowHeight px value from rem value
-    firstRowHeight?.replace("rem", "");
-
     const parent = target.closest("div");
-    console.log("parent", parent);
 
-    // const boundingRect = target?.getBoundingClientRect();
     const boundingRect = parent?.getBoundingClientRect();
     if (!boundingRect) return;
-    console.log("target", target);
-    console.log("boundingRect", boundingRect);
 
     const topMarginElm = parent?.querySelector(".top-margin");
     if (!topMarginElm) return;
@@ -54,40 +44,14 @@ const Events = () => {
       getComputedStyle(topMarginElm).height.replace("px", "")
     );
 
-    // const grandParent = parent?.closest("div");
-    // console.log("gran", grandParent);
     const rightElm = parent?.querySelector(".right-margin");
-
-    console.log("typeof rightElm", typeof rightElm);
-    // check that typeof rightElm is Element
     if (!rightElm) return;
     const rightMargin = parseFloat(
       getComputedStyle(rightElm).width.replace("px", "")
     );
 
-    console.log("rightMargin", rightMargin);
-    console.log("topMargin", topMargin);
-
-    if (!firstRowHeight) return;
-
-    firstRowHeight = parseFloat(firstRowHeight) * 16;
-    // console.log("firstRowHeight", firstRowHeight);
-
-    // console.log("target.style", target.style);
-    // console.log("e.clientX, e.cli", e.clientX, e.clientY);
-
-    // use clientX and clientY to calculate the x and y coordinates of the click relative to the container
-    // the x and y need to take into account the offset of the margin row and column
-    console.log("boundingRect", boundingRect);
-
     const x = e.clientX - boundingRect.left;
     const y = e.clientY - boundingRect.top - topMargin;
-    // console.log("x, y", x, y);
-
-    // get and viewport width and height
-    const viewportWidth = window.innerWidth;
-    const viewportHeight = window.innerHeight;
-    console.log("viewportWidth, viewportHeight", viewportWidth, viewportHeight);
 
     // use the width and height of the container to calculate the day, hour, and minute of the click
     const day = Math.floor((x / (boundingRect.width - rightMargin)) * 7);
@@ -95,11 +59,6 @@ const Events = () => {
     const minute = Math.floor(
       ((y / (boundingRect.height - topMargin)) * 24 - hour) * 60
     );
-
-    console.log("day", day);
-    console.log("hour", hour);
-    console.log("minute", minute);
-    console.log("");
   };
 
   return (
@@ -109,7 +68,7 @@ const Events = () => {
       style={{
         gridTemplateRows: "1.75rem repeat(288, minmax(0, 1fr)) auto",
       }}
-      onClick={handleClick}
+      onClick={void handleClick}
     >
       {events.map((event, i) => (
         <Event key={event.title + i.toString()} {...event} />
